c7faa4119e1c3a40d4ea1bffdb959029ec8607f7
juan9507 <juandavidnaranjo75@gmail.com>
2022-08-22 12:48:03 -0500

    Estructura del proyecto

---
+++ applications/app-service/src/main/java/org/sofka/tour/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package org.sofka.tour.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "org.sofka.tour.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
